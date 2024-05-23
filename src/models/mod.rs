3cdc7f111da9d8a34e9e5ddd2d79fc91a06a7f35
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-05-24 00:33:20 +0300

    init

---
+++ src/models/mod.rs
@@ -0,0 +1,17 @@
+use std::str::FromStr;
+use std::string::ToString;
+use ascii::AsciiString;
+use tiny_http::{Header, HeaderField};
+use once_cell::sync::Lazy;
+
+fn create_ascii_string(s: String) -> AsciiString {
+    match AsciiString::from_str(&*s) {
+        Ok(s) => s,
+        Err(_) => panic!("Failed to create AsciiString"),
+    }
+}
+
+pub static JSON_HEADER: Lazy<Header> = Lazy::new(|| Header {
+    field: HeaderField::from_str("Content-Type").unwrap(),
+    value: create_ascii_string("application/json".to_string()),
+});
