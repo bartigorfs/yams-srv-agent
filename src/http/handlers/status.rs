16e045489ad738cb9c35b35d2b3c2391c438b95a
Igor Bartashevich <bartashevich_i@lwo.by>
2024-08-13 17:02:54 +0300

    feat: remove cpu from status

--- src/http/handlers/status.rs
+++ src/http/handlers/status.rs
@@ -1,16 +1,15 @@
 use std::collections::HashMap;
-use std::ffi::OsString;
+
 use bytes::Bytes;
 use http_body_util::combinators::BoxBody;
 use hyper::{Request, Response, StatusCode};
 use serde_json::{json, Value};
-use sysinfo::{Components, Disks, Networks, RefreshKind, System};
+use sysinfo::System;
+
 use crate::http::service::collect::{collect_component_info, collect_disk_info, collect_memory_info, collect_network_info, collect_process_info};
-use crate::models;
 use crate::models::components::Component;
 use crate::models::disks::DiskInner;
 use crate::models::generic_info::GenericInfo;
-use crate::models::networks::NetworksInner;
 use crate::models::process::ProcessInfo;
 use crate::util::hyper_util::send_json_response;
 
@@ -30,9 +29,6 @@ pub async fn status_handler(req: Request<hyper::body::Incoming>) -> Result<Respo
             "used": generic_info.used,
             "total_swap": generic_info.total_swap,
             "used_swap": generic_info.used_swap
-        },
-        "cpu": {
-            "amount": generic_info.cpu_amount
         },
         "processes": process_info_vec,
         "disks": disks_vec,
